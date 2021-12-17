2678006fce097066e1f0a59b911a4523d7518f0a
Milad Tousi <74304171+miladtousi@users.noreply.github.com>
2021-12-17 17:47:51 +0100

    String Digital Puppet
    There are two scenes for Puppet in the StringDigitalPuppet folder:
    -StringPuppet-HighBuncy
    -StringPuppet-LowBuncy

---
+++ Assets/StringDigitalPuppet/Scripts/LineConnector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineConnector : MonoBehaviour
+{
+    public GameObject[] _obj;
+
+    private LineRenderer line;
+        
+
+
+    void Start()
+    {
+        line = this.gameObject.GetComponent<LineRenderer>();
+        if(_obj.Length>0)
+        line.positionCount = _obj.Length;
+    }
+
+     void Update()
+    {
+        for (int i = 0; i < _obj.Length; i++)
+        {
+            line.SetPosition(i, _obj[i].transform.position);
+        }  
+    }
+}
